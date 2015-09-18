f3840ee53a1a1d89b16db866c048636b6a93c767
Robert Howerton <wh01683@georgiasouthern.edu>
2015-09-18 19:52:53 -0400

    Initial commit

---
+++ src/main/java/SAPAnalyzer.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by robert on 9/18/2015.
+ */
+public class SAPAnalyzer {
+    private JPanel pnlMain;
+    private JTabbedPane tbDatabaseInformation;
+    private JPanel pnlTable;
+    private JPanel pnlTree;
+    private JPanel pnlItemInfo;
+    private JTable tblShownInformation;
+    private JTree treeSAPHierarchy;
+    private JPanel pnlDropboxFilter;
+    private JPanel pnlTableFilter;
+}
