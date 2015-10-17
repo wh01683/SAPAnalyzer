ad14c11396e58cdf22dc24af5e8c49fd2690900c
Robert Howerton <wh01683@georgiasouthern.edu>
2015-10-17 16:23:26 -0400

    Making new form and class to handle part/BOM editing and creation.

---
+++ src/main/java/gui/createforms/EditPart.java
@@ -0,0 +1,34 @@
+package gui.createforms;
+
+import javax.swing.*;
+
+/**
+ * Author: William Robert Howerton III
+ * Created: 10/17/2015
+ */
+public class EditPart {
+    private JPanel pnlMain;
+    private JButton btnFindPart;
+    private JTextField fldPartID;
+    private JTabbedPane tbPart;
+    private JPanel pnlEditPart;
+    private JPanel pnlEditBOM;
+    private JButton btnEdit;
+    private JPanel pnlGeneral;
+    private JPanel pnlStock;
+    private JPanel pnlAdvanced;
+    private JTextField fldPartDesc;
+    private JComboBox cbUnits;
+    private JTextField fldQtyOnHand;
+    private JComboBox cbPartCategory;
+    private JTextField fldPartName;
+    private JTextField fldPartCost;
+    private JTextField fldPartTCost;
+    private JTextField fldPartWaste;
+    private JTextField fldAllocQty;
+    private JTextField fldAvailQty;
+    private JTextField fldMinReordLvl;
+    private JTextField fldLeadTime;
+    private JComboBox fldPartSupplier;
+
+}
